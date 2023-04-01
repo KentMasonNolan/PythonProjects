b6196d3d0b27e742b5f563a311e892cb201251a5
kent <kent.nolan@gmail.com>
2023-04-02 06:56:27 +1200

    added error detection if string is blank

--- LongestWord.py
+++ LongestWord.py
@@ -1,6 +1,8 @@
 def longest_word(text):
     lst = text.split()
     longest = 0
+    if text == '':
+        return ''
     for i in range(0, len(lst)):
         if len(lst[i]) > len(lst[longest]):
             longest = i
@@ -9,3 +11,5 @@ def longest_word(text):
 
 
 longest_word("this is a list of longsword but the longest word is in the middle")
+longest_word('You’ve got two empty halves of coconuts and you’re bangin’ em')
+
