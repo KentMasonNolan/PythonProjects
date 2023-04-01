c38ed89cfc9cf6d8df224c8b537a2b6046ad42e5
kent <kent.nolan@gmail.com>
2023-04-01 22:59:09 +1300

    Added longest word function and added better testing

---
+++ LongestWord.py
@@ -0,0 +1,11 @@
+def longest_word(text):
+    lst = text.split()
+    longest = 0
+    for i in range(0, len(lst)):
+        if len(lst[i]) > longest:
+            longest = i
+    print(lst[longest])
+    return lst[longest]
+
+
+longest_word("this is a list of longsword but the longest word is in the middle")
