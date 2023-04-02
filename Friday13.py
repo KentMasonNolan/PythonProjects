b3e7f9ffe02459d63644bea5208314aa7e5e7030
kent <kent.nolan@gmail.com>
2023-04-03 09:45:19 +1200

    Updated with default functions from w3Schools for reference

--- Friday13.py
+++ Friday13.py
@@ -1,9 +1,31 @@
-import datetime
+# import datetime
+#
+# def friday_the_13th():
+#     x = datetime.datetime.now()
+#
+#     # print(x + timedelta(day=10))
+#
+#
+# Timedelta function demonstration
 
-def friday_the_13th():
-    x = datetime.datetime.now()
+from datetime import datetime, timedelta
 
-    print(x + timedelta(day=10))
+# Using current time
+ini_time_for_now = datetime.now()
 
+# printing initial_date
+print("initial_date", str(ini_time_for_now))
 
-friday_the_13th()
+# Calculating future dates
+# for two years
+future_date_after_2yrs = ini_time_for_now + \
+                         timedelta(days=730)
+
+future_date_after_2days = ini_time_for_now + \
+                          timedelta(days=2)
+
+# printing calculated future_dates
+print('future_date_after_2yrs:', str(future_date_after_2yrs))
+print('future_date_after_2days:', str(future_date_after_2days))
+
+# friday_the_13th()
