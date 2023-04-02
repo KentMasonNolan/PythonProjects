a5cce33fc5d82d42a52c353bbce7bb49680d46f2
kent <kent.nolan@gmail.com>
2023-04-03 11:12:34 +1200

    Updated with Friday13 function to loop through all months until it has found the next friday the 13th. Date is still hard coded

--- Friday13.py
+++ Friday13.py
@@ -1,39 +1,21 @@
-# # import datetime
-# #
-# # def friday_the_13th():
-# #     x = datetime.datetime.now()
-# #
-# #     # print(x + timedelta(day=10))
-# #
-# #
-# # Timedelta function demonstration
-#
-# from datetime import datetime, timedelta
-#
-# # Using current time
-# ini_time_for_now = datetime.now()
-#
-# # printing initial_date
-# print("initial_date", str(ini_time_for_now))
-#
-#
-# def friday_the_13th():
-#     friday = False
-#     dateIncrement = 1
-#     while friday = False:
-#
-#         if ini_time_for_now + timedelta(days=dateIncrement) ==
-#
-#
-#
-# # future_date_after_2yrs = ini_time_for_now + \
-#                          timedelta()
-#
-# future_date_after_2days = ini_time_for_now + \
-#                           timedelta(days=2)
-#
-# # printing calculated future_dates
-# print('future_date_after_2yrs:', str(future_date_after_2yrs))
-# print('future_date_after_2days:', str(future_date_after_2days))
-#
-# # friday_the_13th()
+from datetime import datetime, timedelta
+
+day = 13
+month = 4
+year = 2023
+
+y = datetime.now()
+
+y = datetime(year, month, day)
+
+friday = False
+while friday == False:
+    y = datetime(year, month, day)
+    if month == 12:
+        month = 1
+    else:
+        month += 1
+    if y.strftime("%A") == "Friday":
+        print("Friday the 13th will happen on: ")
+        print(y.strftime("%Y-%m-%d"))
+        friday = True
--- TimeDate_test.py
+++ TimeDate_test.py
@@ -1,15 +1,37 @@
 from datetime import datetime, timedelta
 
+day = 13
+month = 4
+year = 2023
+
 x = datetime.now()
+y = datetime(year, month, day)
 
-print(x.year)
-print(x.strftime("%A"))
+friday = False
+while friday == False:
+    y = datetime(year, month, day)
+    if month == 12:
+        month = 1
+    else:
+        month += 1
+    if y.strftime("%A") == "Friday":
+        print("Friday the 13th will happen on: ")
+        print(str(y))
+        friday = True
+    # print(y.strftime("%A"))
 
-x = x + timedelta(days=2)
-
-print(x.year)
-print(x.strftime("%A"))
-
-if x.strftime("%A") == "Monday":
-    print("Hello")
-else: print("Not Monday")
+# print(x.year)
+# print(x.strftime("%A"))
+#
+# print("Y: ")
+# print(y.strftime("%A"))
+# print(y.strftime("%D"))
+#
+# x = x + timedelta(days=2)
+#
+# print(x.year)
+# print(x.strftime("%A"))
+#
+# if x.strftime("%A") == "Monday":
+#     print("Hello")
+# else: print("Not Monday")
