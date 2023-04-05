91134ca627b1f5d1f74d69b71948cf267a8abb2e
kent <kent.nolan@gmail.com>
2023-04-05 13:01:07 +1200

    Roman to int functions

--- from_roman_numeral.py
+++ from_roman_numeral.py
@@ -1,4 +1,19 @@
 def from_roman_numeral(roman_numeral):
-    romanDic = {'I': 1, 'V': 1,'X': 1,'I': 1,'I': 1,'I': 1,'I': 1,'I': 1,}
+    romanDic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    return
+    answer = 0
+
+    for i in range(len(roman_numeral) - 1):
+
+        if romanDic[roman_numeral[i]] < romanDic[roman_numeral[i + 1]]:
+            answer = answer - romanDic[roman_numeral[i]]
+        else:
+            answer = answer + romanDic[roman_numeral[i]]
+
+    answer = answer + romanDic[roman_numeral[- 1]]
+
+    print(answer)
+    return answer
+
+
+from_roman_numeral('M')
