af05ab77c796df2da06049de6e7a21ac7eb4bc3a
kent <kent.nolan@gmail.com>
2023-04-06 20:42:18 +1200

    Updated anagram function but there is a need to remove the unidecode library

---
+++ is_anagram.py
@@ -0,0 +1,29 @@
+from unidecode import unidecode
+
+def is_anagram(left, right):
+    left = left.replace(" ", "").lower()
+    left = unidecode(left, "utf-8")
+
+    right = right.replace(" ", "").lower()
+    right = unidecode(right, "utf-8")
+
+    leftSet = set(list(left))
+    rightSet = set(list(right))
+    print(leftSet)
+
+
+    if leftSet <= rightSet & rightSet <= leftSet:
+        print("true")
+        return True
+    else:
+        return False
+
+    # defaultCardsList = set(defaultCardsArr)
+    # cardsArr = str.split(cards)
+    # cardsList = set(cardsArr)
+    # missing = (set(defaultCardsList.difference(cardsList)))
+    # missingString = "".join(missing)
+    # return missingString
+
+
+is_anagram("crâné", "crasdhasdcrânéne")
