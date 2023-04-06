eba5c4db433fc49d4f30c7816278b652e4d9f6e7
kent <kent.nolan@gmail.com>
2023-04-07 09:08:21 +1200

    Setting up BreakSafe.py function

---
+++ BreakSafe.py
@@ -0,0 +1,9 @@
+num = [1, 5, 8]
+
+count = 0
+
+for i in range(len(num)):
+    for j in range(len(num)):
+        for k in range(len(num)):
+            for l in range(len(num)):
+                print("pants")
