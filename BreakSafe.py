faf2ce2beacde6a01ae17740e6cecb00ea4c07dd
kent <kent.nolan@gmail.com>
2023-04-07 10:40:55 +1200

    Added nested loops for creating code

--- BreakSafe.py
+++ BreakSafe.py
@@ -1,9 +1,18 @@
 num = [1, 5, 8]
 
+code = [0, 0, 0, 0]
+
+output = ""
+
 count = 0
 
 for i in range(len(num)):
+    code[0] = num[i]
     for j in range(len(num)):
+        code[1] = num[j]
         for k in range(len(num)):
+            code[2] = num[k]
             for l in range(len(num)):
-                print("pants")
+                code[3] = num[l]
+
+
