8b25323af3fc170b1857d7ca671da24dff563003
kent <kent.nolan@gmail.com>
2023-04-08 14:37:30 +1200

    Hopefully final commit

--- FrequencyOfettersInAFile.py
+++ FrequencyOfettersInAFile.py
@@ -1,16 +1,21 @@
 INFILE = open("words.txt", 'r')
 line = INFILE.readline()
 wordlist = line.split()
-TotalCount = 0
+
+letterFreq = [0] * 123
+totalCount = 0
 
 for words in line:
     for characters in words:
         if ord(characters) >= 97 & ord(characters) <= 122:
-            # print(ord(characters))
-            TotalCount += 1
+            totalCount += 1
+            letterFreq[int(ord(characters))] = letterFreq[int(ord(characters))] + 1
 
-print(TotalCount)
-a = "a"
-b = "z"
-print(ord(a))
-print(ord(b))
+
+for i in range(97,123):
+    print(chr(i) + ":", "%.2f" % round(letterFreq[i] / totalCount, 2))
+# for i in range(97,123):
+#     print(chr(i) + ":", round(letterFreq[i] / totalCount, 2))
+
+# print(totalCount)
+
