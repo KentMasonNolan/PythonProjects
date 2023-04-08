e299e1a34d9ded3cde081f460d477dc18d338196
kent <kent.nolan@gmail.com>
2023-04-09 11:50:24 +1200

    Working on error handling

--- DoingHTTPRequests.py
+++ DoingHTTPRequests.py
@@ -1,16 +1,21 @@
 import requests
 
-r = requests.get('https://api.github.com/events', auth=('user', 'pass'))
-
-r = requests.post('https://httpbin.org/post', data={'key': 'value'})
-
-r = requests.options('https://httpbin.org/get')
+try:
+   r = requests.get('https://api.github.com/users/python', auth=('user', 'pass'))
 
 
-print(r.status_code)
-print(r.headers)
-print(r.text)
-print(r.url)
-print(r.cookies)
-print(r.elapsed)
-print(r.json())
+# if r.status_code != 200:
+#     print("No internet connectivity.")
+# else:
+#     print(r.content)
+#
+# r.raise_for_status()
+
+#
+# print(r.status_code)
+# print(r.headers)
+# print(r.text)
+# print(r.url)
+# print(r.cookies)
+# print(r.elapsed)
+# print(r.json())
