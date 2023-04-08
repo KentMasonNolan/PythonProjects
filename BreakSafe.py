474b309bd18bfd500528308a4835b7b3a1555d26
kent <kent.nolan@gmail.com>
2023-04-08 13:23:35 +1200

    Finished product

--- BreakSafe.py
+++ BreakSafe.py
@@ -2,10 +2,6 @@ num = [1, 5, 8]
 
 code = [0, 0, 0, 0]
 
-output = ""
-
-count = 0
-
 for i in range(len(num)):
     code[0] = num[i]
     for j in range(len(num)):
@@ -14,5 +10,7 @@ for i in range(len(num)):
             code[2] = num[k]
             for l in range(len(num)):
                 code[3] = num[l]
+                if set(list(code)) == set(list(num)):
+                    print(' '.join(str(e) for e in code))
 
 
