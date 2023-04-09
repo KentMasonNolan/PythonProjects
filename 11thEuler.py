e36c806a921dcbb6c1049d6ccb9f675dca13bbaf
kent <kent.nolan@gmail.com>
2023-04-09 15:55:11 +1200

    Setup the reader for reading each line and moving to a 2d array

--- 11thEuler.py
+++ 11thEuler.py
@@ -1,6 +1,12 @@
 INFILE = open("11euler.txt", 'r')
 line = INFILE.readline()
-euler = []
+euler = [[None]*20]*20
+for i in range(20):
+    for j in range(20):
+        euler[i][j] = line.split(' ')
+
+    # euler[i] = int,x.strip().split(' ') for x in line
+
 print(euler)
-for i in line:
-    euler.append()
+# for i in line:
+#     euler.append()
