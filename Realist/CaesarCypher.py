b978e48880d22d56aad0990f2baa07a35fd3e844
Kent Nolan <kent.nolan@gmail.com>
2023-04-12 15:12:55 +1200

    Added ability to loop through chr values between 97 & 122 for the function

--- Realist/CaesarCypher.py
+++ Realist/CaesarCypher.py
@@ -1,10 +1,19 @@
 from string import ascii_lowercase
+
 letters = list(ascii_lowercase)
 
-print(letters)
+# print(letters)
 
-a = "a"
-aascii = ord(a)
-aascii += 2
-print(aascii)
+# a = "a"
+# aascii = ord(a)
+# aascii += 2
+# print(aascii)
+
+j = 97
+
+for i in range(97, 150):
+    print(chr(j))
+    j +=1
+    if j%123==0:
+        j=97
 
