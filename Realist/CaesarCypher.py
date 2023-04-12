83ee0a715dcb985f05d4155bad9d85b8eb6b9cf0
kent <kent.nolan@gmail.com>
2023-04-13 07:00:42 +1200

    Setup for next function and layed out how it will work

--- Realist/CaesarCypher.py
+++ Realist/CaesarCypher.py
@@ -2,18 +2,24 @@ from string import ascii_lowercase
 
 letters = list(ascii_lowercase)
 
-# print(letters)
 
-# a = "a"
-# aascii = ord(a)
-# aascii += 2
-# print(aascii)
+def caesar_cypher_encrypt(s, key):
+    output = ""
+    for x in list(s):
+        if ord(x) + key <= 122:
+            print(chr(ord(x) + key))
+            output += chr(ord(x) + key)
+    print(output)
 
-j = 97
+
+caesar_cypher_encrypt("hello world", 3)
+
+print(ord('h') + 3)
+
+# j = 97
 
 # for i in range(97, 150):
 #     print(chr(j))
 #     j +=1
 #     if j%123==0:
 #         j=97
-
