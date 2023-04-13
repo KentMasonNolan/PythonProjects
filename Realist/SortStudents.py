a6273009cbc70d047f66d7b1132d532265e00d73
kent <kent.nolan@gmail.com>
2023-04-13 23:09:02 +1200

    Setup for next HackInScience function

---
+++ Realist/SortStudents.py
@@ -0,0 +1,12 @@
+def sort_by_mark(my_class):
+    ...
+
+
+def sort_by_name(my_class):
+    ...
+
+
+my_class = [(25, "Shannon"), (50, "Alan"), (75, "Ada")]
+
+print(sort_by_mark(my_class))
+print(sort_by_name(my_class))
