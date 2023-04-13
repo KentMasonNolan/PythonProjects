b8cb49083ba69942749922336758640fc82b5834
Kent Nolan <kent.nolan@gmail.com>
2023-04-13 16:40:41 +1200

    Encryption working

--- Realist/CaesarCypher.py
+++ Realist/CaesarCypher.py
@@ -2,9 +2,20 @@ from string import ascii_lowercase
 
 letters = list(ascii_lowercase)
 
+
 def caesar_cypher_encrypt(s, key):
     output = ""
     for x in list(s):
+
+        if 65 <= ord(x) + key <= 90:
+            # print(chr(ord(x) + key))
+            output += chr(ord(x) + key)
+        elif ord(x) + key >= 90 and ord(x) <= 90:
+            y = (ord(x) % 90) + 65
+            # print(y)
+            output += chr(y)
+        elif 65 >= ord(x) <= 90 and 97 >= ord(x) <= 122:
+            output += x
 
         if 97 <= ord(x) + key <= 122:
             # print(chr(ord(x) + key))
@@ -13,8 +24,6 @@ def caesar_cypher_encrypt(s, key):
             y = (ord(x) % 122) + 97
             # print(y)
             output += chr(y)
-        else:
-            output += x
     print(output)
     return output
 
