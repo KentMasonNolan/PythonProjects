1ff6392becbf784e7324412b1dfe57560fbe0311
kent <kent.nolan@gmail.com>
2023-04-13 20:48:08 +1200

    Working but failing unit tests for decrypying "Two", key=13

--- Realist/CaesarCypher.py
+++ Realist/CaesarCypher.py
@@ -6,26 +6,61 @@ letters = list(ascii_lowercase)
 def caesar_cypher_encrypt(s, key):
     output = ""
     for x in s:
+        debug = ord(x)
+
 
         if 65 <= ord(x) + key <= 90:
-            # print(chr(ord(x) + key))
             output += chr(ord(x) + key)
+            continue
+
         elif ord(x) + key >= 90 and ord(x) <= 90:
-            y = (ord(x) % 90) + 65
-            # print(y)
+            y = ((ord(x)+key) % 90) + 64
             output += chr(y)
+            continue
+
         elif 65 >= ord(x) <= 90 and 97 >= ord(x) <= 122:
             output += x
+            continue
 
         if 97 <= ord(x) + key <= 122:
-            # print(chr(ord(x) + key))
             output += chr(ord(x) + key)
+            continue
+
         elif ord(x) + key >= 122:
-            y = (ord(x) % 122) + 97
-            # print(y)
+            y = ((ord(x)+key) % 122) + 96
             output += chr(y)
+            continue
+
     print(output)
     return output
 
 
-caesar_cypher_encrypt("Hello world$$z", 3)
+caesar_cypher_encrypt("Two", 13)
+
+def caesar_cypher_decrypt(s, key):
+
+    output = ""
+    for x in s:
+
+        if 65 <= ord(x) - key <= 90:
+            output += chr(ord(x) - key)
+            continue
+        elif ord(x) - key >= 90 and ord(x) <= 90:
+            y = (ord(x) % 90) + 65
+            output += chr(y)
+            continue
+        elif 65 >= ord(x) <= 90 and 97 >= ord(x) <= 122:
+            output += x
+            continue
+        if 97 <= ord(x) - key <= 122:
+            output += chr(ord(x) - key)
+            continue
+        elif ord(x) - key <= 97:
+            y = 122 - abs((ord(x)-key)-97)
+            output += chr(y)
+
+    print(output)
+    return output
+
+caesar_cypher_decrypt("Gjb rzcgl unyirf bs pbpbahgf", 13)
+
