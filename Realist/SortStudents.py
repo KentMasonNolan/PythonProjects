b3520fc2440977dd682753e5e794f428dddb44d3
Kent Nolan <kent.nolan@gmail.com>
2023-04-14 15:55:06 +1200

    Added SortStudents.py function for HackInScience

--- Realist/SortStudents.py
+++ Realist/SortStudents.py
@@ -1,9 +1,11 @@
 def sort_by_mark(my_class):
-    ...
+    output = sorted(my_class, reverse=True)
+    return output
 
 
 def sort_by_name(my_class):
-    ...
+    output = sorted(my_class, key=lambda a: a[1])
+    return output
 
 
 my_class = [(25, "Shannon"), (50, "Alan"), (75, "Ada")]
