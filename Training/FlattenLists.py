221485417174bdf8ff591899f6b26a89b28b2152
kent <kent.nolan@gmail.com>
2023-04-15 21:51:09 +1200

    Added and new function and now error testing

---
+++ Training/FlattenLists.py
@@ -0,0 +1,13 @@
+def flatten(a_list):
+    outputList = []
+    for i in range(len(a_list)):
+        for j in range(len(a_list[i])):
+
+            outputList.append(a_list[i][j])
+    print(outputList)
+    return outputList
+
+
+g = [[1, 2, 4], [], [1, 2, 3, 4], [2, 3, 4, 5]]
+
+
