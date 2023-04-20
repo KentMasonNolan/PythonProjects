e41668fe4da49fb8a2ac29e8ea223e0389e2b3a4
kent <kent.nolan@gmail.com>
2023-04-20 19:11:08 +1200

    Error testing and finding out where I was up to last

--- Realist/is_anagram.py
+++ Realist/is_anagram.py
@@ -1,29 +1,45 @@
-from unidecode import unidecode
+# def is_anagram(left, right):
+#     left = left.replace(" ", "").lower()
+#
+#     right = right.replace(" ", "").lower()
+#
+#     leftList = list(left)
+#     for i in leftList:
+#         if i.isalnum():
+#             leftList[i]
+#     print(leftList)
+#
+#     leftSet = set(list(left))
+#     rightSet = set(list(right))
+#     # print(leftSet)
+#
+#
+#     if leftSet <= rightSet & rightSet <= leftSet:
+#         print("true")
+#         return True
+#     else:
+#         return False
+#
+#     # defaultCardsList = set(defaultCardsArr)
+#     # cardsArr = str.split(cards)
+#     # cardsList = set(cardsArr)
+#     # missing = (set(defaultCardsList.difference(cardsList)))
+#     # missingString = "".join(missing)
+#     # return missingString
+#
+#
+# is_anagram("crâné", "crâné")
 
-def is_anagram(left, right):
-    left = left.replace(" ", "").lower()
-    left = unidecode(left, "utf-8")
-
-    right = right.replace(" ", "").lower()
-    right = unidecode(right, "utf-8")
-
-    leftSet = set(list(left))
-    rightSet = set(list(right))
-    print(leftSet)
 
 
-    if leftSet <= rightSet & rightSet <= leftSet:
-        print("true")
-        return True
-    else:
-        return False
+import unicodedata
 
-    # defaultCardsList = set(defaultCardsArr)
-    # cardsArr = str.split(cards)
-    # cardsList = set(cardsArr)
-    # missing = (set(defaultCardsList.difference(cardsList)))
-    # missingString = "".join(missing)
-    # return missingString
+def is_anagram(str1, str2):
+    str1 = unicodedata.normalize('NFKD', str1.lower().replace(" ", "").replace("'", "").replace("-", "").replace('"', ''))
+    str2 = unicodedata.normalize('NFKD', str2.lower().replace(" ", "").replace("'", "").replace("-", "").replace('"', ''))
+    str1 = ''.join(sorted(str1, key=lambda s: unicodedata.normalize('NFKD', s)))
+    str2 = ''.join(sorted(str2, key=lambda s: unicodedata.normalize('NFKD', s)))
+    return str1 == str2
 
 
-is_anagram("crâné", "crasdhasdcrânéne")
+is_anagram("nectar","cranté")
