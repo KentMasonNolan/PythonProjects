9a93a70c1d983915f171dfeaeca32527c10628c3
kent <kent.nolan@gmail.com>
2023-05-11 08:26:39 +1200

    Added DSA

---
+++ DSA/BubbleSort.py
@@ -0,0 +1,41 @@
+import random
+
+
+def BubbleSort(toSort):
+    bubbleSorted = False
+    listLength = len(toSort) - 1
+
+    while not bubbleSorted:
+        bubbleSorted = True
+        for i in range(listLength):
+            if toSort[i] < toSort[i + 1]:
+                toSort[i], toSort[i + 1] = toSort[i + 1], toSort[i]
+                bubbleSorted = False
+            bubbleSorted -= 1
+    print(toSort)
+    return toSort
+
+def bubble_sort(list):
+    unsorted_until_index = len(list) - 1
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(unsorted_until_index):
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+                sorted = False
+            unsorted_until_index -= 1
+    return list
+
+# BubbleSort()
+
+print(bubble_sort([65, 55, 45, 35, 25, 15, 10]))
+
+BubbleSort([65, 55, 45, 35, 25, 15, 10])
+
+for i in range(10):
+    returnList=[]
+    returnList[i] = random.randrange(1, 100, 1)
+    print(returnList[i])
+
+print(returnList)
--- Realist/is_anagram.py
+++ Realist/is_anagram.py
@@ -1,45 +1,30 @@
-# def is_anagram(left, right):
-#     left = left.replace(" ", "").lower()
-#
-#     right = right.replace(" ", "").lower()
-#
-#     leftList = list(left)
-#     for i in leftList:
-#         if i.isalnum():
-#             leftList[i]
-#     print(leftList)
-#
-#     leftSet = set(list(left))
-#     rightSet = set(list(right))
-#     # print(leftSet)
-#
-#
-#     if leftSet <= rightSet & rightSet <= leftSet:
-#         print("true")
-#         return True
-#     else:
-#         return False
-#
-#     # defaultCardsList = set(defaultCardsArr)
-#     # cardsArr = str.split(cards)
-#     # cardsList = set(cardsArr)
-#     # missing = (set(defaultCardsList.difference(cardsList)))
-#     # missingString = "".join(missing)
-#     # return missingString
-#
-#
-# is_anagram("crâné", "crâné")
+import unidecode
+
+def is_anagram(left, right):
+    left = left.replace(" ", "").lower()
+    left = unidecode(left, "utf-8")
+
+    right = right.replace(" ", "").lower()
+    right = unidecode(right, "utf-8")
+
+    leftSet = set(list(left))
+    rightSet = set(list(right))
+    # print(leftSet)
 
 
+    if leftSet <= rightSet & rightSet <= leftSet:
+        print("true")
+        return True
+    else:
+        print("False")
+        return False
 
-import unicodedata
-
-def is_anagram(str1, str2):
-    str1 = unicodedata.normalize('NFKD', str1.lower().replace(" ", "").replace("'", "").replace("-", "").replace('"', ''))
-    str2 = unicodedata.normalize('NFKD', str2.lower().replace(" ", "").replace("'", "").replace("-", "").replace('"', ''))
-    str1 = ''.join(sorted(str1, key=lambda s: unicodedata.normalize('NFKD', s)))
-    str2 = ''.join(sorted(str2, key=lambda s: unicodedata.normalize('NFKD', s)))
-    return str1 == str2
+    # defaultCardsList = set(defaultCardsArr)
+    # cardsArr = str.split(cards)
+    # cardsList = set(cardsArr)
+    # missing = (set(defaultCardsList.difference(cardsList)))
+    # missingString = "".join(missing)
+    # return missingString
 
 
-is_anagram("nectar","cranté")
+is_anagram("crâné", "crasdhasdcrânéne")
