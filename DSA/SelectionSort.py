7dde0e71a132852fc8224954619511e3b96fc91f
kent <kent.nolan@gmail.com>
2023-05-12 11:19:33 +1200

    Added selection Sort

---
+++ DSA/SelectionSort.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+
+
+def selectionSort(inputList):
+    sorted = False
+    finalIndex = len(inputList) - 1
+    startIndex = 0
+
+    while not sorted:
+        sorted = True
+
+        while finalIndex != 1:
+            for i in range(finalIndex):
+                if inputList[startIndex] > inputList[startIndex + 1] and inputList[startIndex] != finalIndex:
+                    inputList[startIndex], inputList[startIndex + 1] = inputList[startIndex + 1], inputList[startIndex]
+                startIndex += 1
+            finalIndex -= 1
+            startIndex = 0
+
+    return inputList
+
+
+print(selectionSort([65, 45, 25, 55, 15, 10, 35]))
+
+randnums = np.random.randint(1, 101, 100)
+
+print("Before sort")
+print(randnums)
+print("After sort")
+print(selectionSort(randnums))
